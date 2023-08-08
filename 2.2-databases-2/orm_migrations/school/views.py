23a5cc2cb29169fe490eae54aaea946b5a3eaa75
Daniil Mededev <daniel.medvedev@gmail.com>
2023-08-08 16:13:52 +0300

    Work with ORM 2 commite finished

--- 2.2-databases-2/orm_migrations/school/views.py
+++ 2.2-databases-2/orm_migrations/school/views.py
@@ -5,11 +5,11 @@ from .models import Student
 
 
 def students_list(request):
-    template = 'school/students_list.html'
-    context = {}
+    template = "school/students_list.html"
 
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-    ordering = 'group'
-
+    ordering = "group"
+    object_list = Student.objects.all().prefetch_related("teachers").order_by(ordering)
+    context = {"object_list": object_list}
     return render(request, template, context)
