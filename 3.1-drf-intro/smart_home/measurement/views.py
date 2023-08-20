bcf36eb0ddbf79549dd57ef1feae4d0c25f9d740
Daniil Mededev <daniel.medvedev@gmail.com>
2023-08-20 19:21:18 +0300

    Work with DRF commite one

--- 3.1-drf-intro/smart_home/measurement/views.py
+++ 3.1-drf-intro/smart_home/measurement/views.py
@@ -1,2 +1,26 @@
-# TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
-# TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+from rest_framework.generics import (
+    ListCreateAPIView,
+    RetrieveUpdateAPIView,
+    CreateAPIView,
+)
+from .models import Sensor, Measurement
+from .serializers import (
+    SensorSerializer,
+    SensorDetailSerializer,
+    MeasurementCreateSerializer,
+)
+
+
+class SensorCreateView(ListCreateAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorSerializer
+
+
+class SensorDetailView(RetrieveUpdateAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorDetailSerializer
+
+
+class MeasurementCreateView(ListCreateAPIView):
+    queryset = Measurement.objects.all()
+    serializer_class = MeasurementCreateSerializer
