a6b2862e126d58df5c55b825d2369e10c9908e0d
Daniil Medvedev <daniel.medvedev@gmail.com>
2023-09-29 15:00:46 +0300

    4 commit

--- 3.2-crud/stocks_products/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /home/medy/Task_django/3.2-crud/stocks_products
-git pull origin video
-source env/bin/activate
-python manage.py migrate
-deactivate
-sudo systemctl restart gunicorn  
